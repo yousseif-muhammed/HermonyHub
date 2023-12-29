59434689a3e4a1eaaefe44779f67c628c81fbfd6
yousseif-muhammed <155190514+yousseif-muhammed@users.noreply.github.com>
2023-12-29 21:59:18 +0200

    Update __init__.py

--- FallenMusic/__init__.py
+++ FallenMusic/__init__.py
@@ -46,18 +46,14 @@ LOGGER = logging.getLogger("FallenMusic")
 app = Client(
     "FallenMusic",
     config.API_ID,
-    config.API_HASH,
+    config.API_HAsH,
     bot_token=config.BOT_TOKEN,
 )
 
 app2 = Client(
     "FallenAss",
     api_id=config.API_ID,
-<<<<<<< HEAD
-    API_HAsH=config.API_HAsH,
-=======
-    api_hash=config.API_HASH,
->>>>>>> refs/remotes/origin/main
+    api_hash=config.API_HAsH,
     session_string=str(config.SESSION),
 )
 
