668ce804b8209fa67e59f460bd9c74af75519350
yousseif-muhammed <155190514+yousseif-muhammed@users.noreply.github.com>
2024-01-08 16:01:53 +0200

    Update search.py

--- FallenMusic/Modules/search.py
+++ FallenMusic/Modules/search.py
@@ -23,8 +23,19 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtube_search import YoutubeSearch
+from urllib.parse import parse_qs, urlparse
 
 from FallenMusic import app
+
+
+def convert_youtube_link(original_link):
+    parsed_url = urlparse(original_link)
+    video_id = parse_qs(parsed_url.query).get('v')
+
+    if video_id:
+        return f"https://www.youtube.com/watch?v={video_id[0]}"
+    else:
+        return None
 
 
 @app.on_message(filters.command(["search"]))
@@ -33,21 +44,35 @@ async def ytsearch(_, message: Message):
         await message.delete()
     except:
         pass
+
     try:
         if len(message.command) < 2:
-            return await message.reply_text("» give some text to search baby !")
+            return await message.reply_text("» Give some text to search baby!")
+        
         query = message.text.split(None, 1)[1]
         m = await message.reply_text("🔎")
+        
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
+        
         while i < 4:
-            text += f"title : {results[i]['title']}\n"
-            text += f"duration : `{results[i]['duration']}`\n"
-            text += f"views : `{results[i]['views']}`\n"
-            text += f"channel : {results[i]['channel']}\n"
-            text += f"link : https://youtube.com{results[i]['url_suffix']}\n\n"
+            title = results[i]['title']
+            duration = results[i]['duration']
+            views = results[i]['views']
+            channel = results[i]['channel']
+            
+            link = convert_youtube_link(f"https://youtube.com{results[i]['url_suffix']}")
+            
+            if link:
+                text += f"title : {title}\n"
+                text += f"duration : `{duration}`\n"
+                text += f"views : `{views}`\n"
+                text += f"channel : {channel}\n"
+                text += f"link : {link}\n\n"
+            
             i += 1
+        
         key = InlineKeyboardMarkup(
             [
                 [
@@ -58,10 +83,12 @@ async def ytsearch(_, message: Message):
                 ]
             ]
         )
+        
         await m.edit_text(
             text=text,
             reply_markup=key,
             disable_web_page_preview=True,
         )
+    
     except Exception as e:
         await message.reply_text(str(e))
