150f7f9f0388b00ed337e356c90694c1b241742c
yousseif-muhammed <155190514+yousseif-muhammed@users.noreply.github.com>
2024-01-08 15:58:38 +0200

    Update song.py

--- FallenMusic/Modules/song.py
+++ FallenMusic/Modules/song.py
@@ -64,7 +64,7 @@ async def song(_, message: Message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"Title :** [{title[:23]}]({link})\\Duration :** `{duration}`\nBy :** {BOT_MENTION}"
+        rep = f"Title :** [{title[:23]}]({link})\nDuration :** `{duration}`\nBy :** {BOT_MENTION}"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
